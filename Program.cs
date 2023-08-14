5a665cf8cf544e95c2581878572c1fdb01bc0a3a
reshadsaidov <alhadss@code.edu.az>
2023-08-14 12:50:13 +0400

    fir commit

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using System.Dynamic;
+
+namespace ConsoleApp1
+{
+
+
+
+
+    public class PhotoBook
+    {
+
+
+
+        public PhotoBook()
+        {
+
+            NumPages = 16;
+        }
+        public PhotoBook(int numPages )
+        {
+
+            NumPages = numPages;
+        }
+
+        private int NumPages { get; set; }
+
+        public int GetNumberPages()
+        {
+            
+            return NumPages;
+        }
+
+   
+    }
+
+    public class BigPhotoBook
+    {
+
+        public BigPhotoBook()
+        {
+
+            NumPages = 64;
+        }
+        private int NumPages { get; set; }
+        public int GetNumberPages()
+        {
+
+            return NumPages;
+        }
+
+
+    }
+
+
+
+    public class Program
+    {
+        public static void Main()
+        {
+            PhotoBook book1 = new PhotoBook();   
+           Console.WriteLine( book1.GetNumberPages());
+            PhotoBook book2 = new PhotoBook(24);
+            Console.WriteLine(book2.GetNumberPages());
+            BigPhotoBook bigPhotoBook1 = new BigPhotoBook();
+            Console.WriteLine(bigPhotoBook1.GetNumberPages());
+        }
+    }
+
+
+
+
+}
